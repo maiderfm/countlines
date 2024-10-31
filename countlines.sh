f877358b0eb56f62b5ac369a9d15cfc6c38d11f1
Maider <maiderupvehu@gmail.com>
2024-10-31 13:31:43 +0100

    Multiple arguments

--- countlines.sh
+++ countlines.sh
@@ -1,12 +1,15 @@
+for file_name in "$@"
+do
+    echo "$file_name"
+    n_lines=$(cat "$file_name" | wc -l)
 
-echo "$1"
-n_lines=$(cat "$1" | wc -l)
-if  [[ $n_lines -eq 0 ]];
-then
-echo "$1" has 0 lines
-elif [[ $n_lines -eq 1 ]];
-then
-echo "$1" has 1 line
-else
-echo "$1" has $n_lines lines
-fi
+	if  [[ $n_lines -eq 0 ]];
+	then
+		echo "$file_name" has 0 lines
+	elif [[ $n_lines -eq 1 ]];
+	then
+		echo "$file_name" has 1 line
+	else
+		echo "$file_name" has $n_lines lines
+	fi
+done
